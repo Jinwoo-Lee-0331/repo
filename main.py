25d1bd603d2fb6fc1db00823819979479689b88f
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-01-09 22:58:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def runqry(date_i,loc_i):
     # cursor = conn.cursor()
     query = "SELECT Time, Tag, Value FROM RawData where Time > '" + date_i.strftime("%Y-%m-%d") + " 07:00:00' and Time < '" + \
             date_i.strftime("%Y-%m-%d") + " 21:00:00' and tag like '%" + loc_i + "%' order by Time asc;"
-    x = conn.query(query1)
+    x = conn.query(query)
     x = pd.DataFrame(x)
     # x = x[x["TAG"].str.contains(r'(OPC UA.(\w+).2.Tags.\w+.\w+.(\w+)-(\w-\d\w)-(.+))')]
     y = pd.concat([x["Time"], x["Tag"].str.extract(r'(\w+)-(\w+)-(\w-\w+)-(.+)'),
