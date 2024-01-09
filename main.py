69f8ddeb1c070f01fbae2700165ab1082268334c
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-01-09 21:43:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def init_connection():
         url=f"mysql://{username}:{user_password}@{host}:{tunnel.local_bind_port}/{database}?charset=utf8mb4")
 
 conn=init_connection()
+st.write(tunnel.local_bind_port)
 
 # hrs=pd.read_csv('./data/hrs.csv',header=None)
 hrs=pd.read_csv('C:\\Users\\researcher\\Desktop\\hrs.csv',header=None)
