5d2ece4ec00414bf904eeb0d5ee7d114e61b799f
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-01-13 23:14:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,21 +46,21 @@ hrs=pd.read_csv('./data/hrs.csv',header=None)
 hrs.columns=['Location','Address']
 hrs['Last Connected Time']='Disconnected'
 
+
 @st.cache_data(ttl=600)
 def streamlit_init(hrs):
     for idx,i in enumerate(hrs['Location']):
-        query1 = (f"SELECT Time ,Tag ,Value FROM RawData"
-                  f" where Tag like '%{i}%온도%' order by Time desc LIMIT 1;")
-        # qry= pd.read_sql(query1, conn)
-        qry=conn.query(query1)
-        qry=pd.DataFrame(qry)
+        query1 = (f"SELECT TOP (1) [Time] ,[Tag] ,[Value] FROM [hmcportal].[dbo].[RawData]"
+                  f" where Tag like '%{i}%온도%' and Time > '{(datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')}' order by Time desc")
+        qry= conn.query(query1)
+        qry=pd.DataFrame(qry)        
         try:
             hrs.loc[idx,'Last Connected Time']=qry.loc[0,'Time'].strftime("%Y-%m-%d %H:%M:%S")
         except Exception as e:
             print(e)
     return hrs
 
-st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 def runqry(date_i,loc_i):
     query = "SELECT Time, Tag, Value FROM RawData where Time > '" + date_i.strftime("%Y-%m-%d") + " 07:00:00' and Time < '" + \
             date_i.strftime("%Y-%m-%d") + " 21:00:00' and tag like '%" + loc_i + "%' order by Time asc;"
@@ -71,31 +71,53 @@ def runqry(date_i,loc_i):
                    x["Value"]], axis=1)
     y.columns = ["Time", "Location", "Attribute","Serial", "Tag", "Value"]
     x['Legend']=y['Tag']
-    # 연결 끊기
-    # conn.close()
-    return x, y
+    z = y["Tag"]
+    z = pd.concat([z, z], axis=1)
+    z.drop_duplicates(inplace=True)
+    z.columns = ["label", "value"]
+    z = z.to_dict('records')
+
+    srl = y['Serial'].drop_duplicates()
+    srl_trd = []
+    for idx_i, i in enumerate(srl):
+        atr = y.loc[y['Serial'] == i, 'Attribute'].drop_duplicates()
+        atr_trd = []
+        for idx_j, j in enumerate(atr):
+            tag = y.loc[(y['Serial'] == i) & (y['Attribute'] == j), 'Tag'].drop_duplicates()
+            tag_trd = []
+            for k in tag:
+                tag_trd.append({'label': k, 'value': j + '-' + i + '-' + k})
+            atr_trd.append({'label': j, 'value': j + '-' + i, 'children': tag_trd})
+        srl_trd.append({'label': i, 'value': i, 'children': atr_trd})
+    root = [{'label': loc_i, 'value': loc_i, 'children': srl_trd}]
+    return x, y, root
 
 if 'key' not in st.session_state:
     st.session_state.key = False
 if 'plot' not in st.session_state:
     st.session_state['plot'] = False
+if 'update_key' not in st.session_state:
+    st.session_state['update_key'] = False
 if 'update' not in st.session_state:
-    st.session_state['update'] = True
+    st.session_state['update'] = False
 
 col1, col2 = st.columns(2)
 with col1:
-    hometab, tab2  = st.tabs(["📋 Board", "📊 Operation"])
+    hometab, tab2 = st.tabs(["📋 Board", "📊 Operation"])
 
 with hometab:
     if st.button(label="Update", use_container_width=True):
-        # hrs = streamlit_init(hrs)
-        streamlit_init(hrs).to_csv('./data/hrs_update.csv', index=False, encoding='utf-8')
-        st.session_state['update'] = True
+        hrs_update=streamlit_init(hrs)
+        st.session_state['update_key'] = True
+        st.session_state['update'] = hrs_update
 
-    if st.session_state['update']:
-        hrs_update = pd.read_csv('./data/hrs_update.csv')
-        hometab.table(hrs_update[['Location','Last Connected Time','Address']])
-
+    if st.session_state['update_key']:
+        hometab.table(st.session_state['update'])
+        # try:
+        #     hometab.table(hrs_update)
+        #     # st.session_state['update'] = False
+        # except Exception as e:
+        #     st.write(e)
 
 with col2:
     tab1, tab3  = st.tabs(["📈 Chart","❗ Alarm"])
@@ -113,31 +135,8 @@ with st.sidebar:
         st.session_state.key = True
         st.session_state['plot'] = True
     st.markdown("---")
-
     if st.session_state.key:
-        x, y = runqry(date_i, loc_i)
-        z = y["Tag"]
-        z = pd.concat([z, z], axis=1)
-        z.drop_duplicates(inplace=True)
-        z.columns = ["label", "value"]
-        z = z.to_dict('records')
-
-        srl = y['Serial'].drop_duplicates()
-        srl_trd = []
-        for idx_i,i in enumerate(srl):
-            atr = y.loc[y['Serial'] == i, 'Attribute'].drop_duplicates()
-            atr_trd = []
-            for idx_j,j in enumerate(atr):
-                tag = y.loc[(y['Serial'] == i) & (y['Attribute'] == j), 'Tag'].drop_duplicates()
-                tag_trd = []
-                for k in tag:
-                    tag_trd.append({'label': k, 'value': j+'-'+i+'-'+k})
-                atr_trd.append({'label': j, 'value': j+'-'+i, 'children': tag_trd})
-            srl_trd.append({'label': i, 'value': i, 'children': atr_trd})
-        root=[{'label': loc_i, 'value': loc_i, 'children': srl_trd}]
-        return_select = tree_select(root, checked=[root[0]['children'][0]['children'][0]['children'][0]['value']],
-                                    expanded=[root[0]['value'], root[0]['children'][0]['value'],
-                                              root[0]['children'][0]['children'][0]['value']])
+        x, y, root = runqry(date_i, loc_i)
         opr=y[(y["Attribute"]=='STS')]
         opr['Value']=opr['Value'].astype(bool)
         opr.set_index("Time",drop=True,inplace=True)
@@ -147,10 +146,16 @@ with st.sidebar:
         tab2.dataframe(opr)
         tab3.dataframe(alm)
 
+    if st.session_state['plot']:
+        return_select = tree_select(root,checked=[root[0]['children'][0]['children'][0]['children'][0]['value']],
+                                    expanded=[root[0]['value'], root[0]['children'][0]['value'],
+                                   root[0]['children'][0]['children'][0]['value']])
+
 if st.session_state['plot']:
     if return_select["checked"]:
-        y2=x.loc[x["Tag"].str.contains('|'.join(return_select["checked"])), ["Time", "Tag", "Value","Legend"]].reset_index(drop=True, inplace=True)
+        y2=x.loc[x["Tag"].str.contains('|'.join(return_select["checked"]))]
+        y2.reset_index(drop=True,inplace=True)
         try:
-            tab1.line_chart(y2,x='Time',y='Value',color='Legend')
+            tab1.line_chart(y2, x='Time',y='Value',color='Legend')
         except Exception as e:
-            tab1.line_chart(y2,x='Time',y='Value')
+            st.write(e)
