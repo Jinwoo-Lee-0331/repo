81720808a21688a91a85309ededcb2a9a871b2ea
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-01-30 21:52:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,25 +35,25 @@ def main():
     if "processComplete" not in st.session_state:
         st.session_state.processComplete = None
 
-    with st.sidebar:
+    # with st.sidebar:
         # uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx'], accept_multiple_files=True)
         # folder_path= 'C:/Users/researcher/Desktop/streamlit/data/document/topdf/'
-        folder_path='./data/'
-        uploaded_files = os.listdir(folder_path)
-        openai_api_key = "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"
-        process = st.button("Process")
-    if process:
-        if not openai_api_key:
-            st.info("Please add your OpenAI API key to continue.")
-            st.stop()
+    folder_path='./data/'
+    uploaded_files = os.listdir(folder_path)
+    openai_api_key = "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"
+        # process = st.button("Process")
+    # if process:
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-        files_text = get_text(uploaded_files)
-        text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
+    files_text = get_text(uploaded_files)
+    text_chunks = get_text_chunks(files_text)
+    vetorestore = get_vectorstore(text_chunks)
 
-        conversation = get_conversation_chain(vetorestore, openai_api_key)
+    conversation = get_conversation_chain(vetorestore, openai_api_key)
 
-        st.session_state.processComplete = True
+    st.session_state.processComplete = True
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{"role": "assistant",
