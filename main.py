5af86bc8b703fd2d67c16ce2f9c4232fb694ae1c
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-03-03 02:14:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,13 @@ from langchain.embeddings import HuggingFaceEmbeddings
 
 from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores import FAISS
+
+from audio_recorder_streamlit import audio_recorder
+from pydub import AudioSegment
+from io import BytesIO
     
 def main():
-
     st.title("💧 _KGT Chatbot_ ")
-
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
@@ -41,14 +43,28 @@ def main():
     cont=st.container()
     with st.sidebar:
         openai_api_key = st.secrets["api_key"]
-        genre = st.radio(
-            "Chat Type",
-            ["***Text Generation***","***Document based***", "***Imaga Generation***", "***Vision***", "***Image Edit***"],
-            index=0,
-        )
+        gr, tg = st.columns([3,2])
+        with gr:
+            genre = st.radio(
+                "Chat Type",
+                ["***Text Generation***","***Document based***", "***Vision***", "***Imaga Generation***", "***Image Edit***", "***Text to Speech***", "***Speech to Text***"],
+                captions=['일반적인 TEXT 답변','문서 기반 답변', '이미지 기반 답변', '이미지 생성(DALL-E-3)', '이미지 편집(DALL-E-2)', '음성 생성', '음성 인식'],    
+                index=0,
+            )            
+                
+        with tg:
+            st.markdown(" ")
+            st.markdown(" ")
+            togg=st.toggle("***Record Chat***")
+            st.markdown("", help="회사망 사용 불가")
         st.divider()
+    
         if genre == "***Text Generation***":
             st.write('')
+        elif genre == "***Imaga Generation***":       
+            st.write('')     
+        elif genre == "***Text to Speech***":     
+            voice = st.radio("Voice",['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'],index=0)
         elif genre == "***Document based***":
             uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx'], accept_multiple_files=True)
             if uploaded_files:
@@ -58,8 +74,6 @@ def main():
                     vetorestore = get_vectorstore(text_chunks)
                     st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key)
                     st.session_state.processComplete = True
-        elif genre == "***Imaga Generation***":       
-            st.write('')     
         elif genre == "***Vision***":  
             uploaded_image = st.file_uploader("Upload your Image", type=['png', 'jpg', 'jpeg', 'gif'], accept_multiple_files=False)            
             # st.write(uploaded_image)
@@ -90,17 +104,62 @@ def main():
                 alpha.paste(255 ,(0,0,alpha.size[0],alpha.size[1]))
                 alpha.paste(0, (left,top,left+width,top+height))
                 img_crop.putalpha(alpha)
-                # st.image(img_crop)
+                # st.write(f"{img.size[0]}x{img.size[1]}")
+        elif genre == "***Speech to Text***": 
+            audio_bytes = audio_recorder(
+                    text="Click to Record",
+                    recording_color="#e8b62c",
+                    neutral_color="#6aa36f",
+                    # icon_name="user",
+                    icon_size="2x",
+                )
+            uploaded_audio = st.file_uploader("Upload your Audio", type=['mp3','wav','mpeg','webm','mp4','m4a'], accept_multiple_files=False)
+            
+    st.chat_message("assistant").write("안녕하세요? 궁금한 것이 있으면 물어보세요")
+    
+    if genre == "***Speech to Text***": 
+        if audio_bytes:
+            st.chat_message("user").audio(audio_bytes)
+            audio_bytes = AudioSegment(data=audio_bytes).export('./test.wav',format='wav')
+    
+            client = OpenAI(api_key=openai_api_key)                
+            transcript = client.audio.transcriptions.create(
+              model="whisper-1",
+              file=audio_bytes,
+                language='ko',
+                temperature=0.1
+            )
+            msg = transcript.text
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)        
+        
+        if uploaded_audio:
+            st.chat_message("user").audio(uploaded_audio, format="audio/wav") 
+    
+            client = OpenAI(api_key=openai_api_key)                
+            transcript = client.audio.transcriptions.create(
+              model="whisper-1",
+              file=uploaded_audio
+            )
+            msg = transcript.text
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)      
 
+    ############# session_state ###############
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요? 궁금한 것이 있으면 물어보세요"}]
+    if togg:
+        if "messages" in st.session_state:
+            for messages in st.session_state.messages:
+                if len(messages["content"]) > 5000:
+                    st.chat_message(messages["role"]).audio(messages["content"])   
+                elif messages["content"][0:4]=='http':
+                    st.chat_message(messages["role"]).image(messages["content"])
+                else:
+                    st.chat_message(messages["role"]).write(messages["content"])
 
-    for msg in st.session_state.messages:
-        if msg["content"][0:4]=='http':
-            st.chat_message(msg["role"]).image(msg["content"])
-        else:
-            st.chat_message(msg["role"]).write(msg["content"])
-
+    ############# chat ###############
+    
     if prompt := st.chat_input():
         client = OpenAI(api_key=openai_api_key)
         if not openai_api_key:
@@ -118,6 +177,7 @@ def main():
                     size="1024x1024")
                 st.session_state.messages.append({"role": "assistant", "content": response.data[0].url})
                 st.chat_message("assistant").image(response.data[0].url)
+                
             elif genre == "***Vision***":
                 payload = {
                   "model": "gpt-4-vision-preview",
@@ -144,6 +204,7 @@ def main():
                 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
                 st.session_state.messages.append({"role": "assistant", "content": response.json()['choices'][0]['message']['content']})
                 st.chat_message("assistant").write(response.json()['choices'][0]['message']['content'])
+                
             elif genre == "***Document based***":
                 # response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
                 # msg = response.choices[0].message.content
@@ -164,26 +225,57 @@ def main():
                 with st.expander("참고 문서 확인"):
                     st.markdown(source_documents[0].metadata['source'], help=source_documents[0].page_content)
                     st.markdown(source_documents[1].metadata['source'], help=source_documents[1].page_content)
-            elif genre == "***Image Edit***":
-                img.save('img.png')
-                img_crop.save('img_crop.png')
-                img=open('img.png','rb')
-                img_crop=open('img_crop.png','rb')
+                    
+            elif genre == "***Image Edit***":              
+                
+                # img.save('img.png')
+                # img_crop.save('img_crop.png')
+                # img=open(img,'rb')
+                # img_crop=open(img_crop,'rb')
+                byte_img = io.BytesIO()
+                img.save(byte_img, format='png')
+                byte_img_crop = io.BytesIO()
+                img_crop.save(byte_img_crop, format='png')
                 response=client.images.edit(
-                    model='dall-e-3',
-                  image=img,
-                  mask=img_crop,
+                  image=byte_img,
+                  mask=byte_img_crop,
                   prompt=prompt,
+                    # size = f"{img.size[0]}x{img.size[1]}",
                   n=1,
                 )
                 st.session_state.messages.append({"role": "assistant", "content": response.data[0].url})
                 st.chat_message("assistant").image(response.data[0].url)
-            else:                    
-                response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
+                
+            elif genre == "***Text to Speech***":
+                response = client.audio.speech.create(
+                    model="tts-1-hd",
+                    voice=voice,
+                    input=prompt,
+                )
+                msg = response.read()
+                st.session_state.messages.append({"role": "assistant", "content": msg, "genre": 'tts'})
+                st.chat_message("assistant").audio(msg)
+
+            elif genre == "***Speech to Text***":
+                transcript = client.audio.transcriptions.create(
+                  model="whisper-1",
+                  file=audio_bytes.read()
+                )
+                msg = transcript.text
+                st.session_state.messages.append({"role": "assistant", "content": msg})
+                st.chat_message("assistant").write(msg)
+                
+            else:
+                response = client.chat.completions.create(model="gpt-4", messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt}])
                 msg = response.choices[0].message.content
                 st.session_state.messages.append({"role": "assistant", "content": msg})
                 st.chat_message("assistant").write(msg)
                 
+
+############# document based def ###############
+
 def tiktoken_len(text):
     tokenizer = tiktoken.get_encoding("cl100k_base")
     tokens = tokenizer.encode(text)
