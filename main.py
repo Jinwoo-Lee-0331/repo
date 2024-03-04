089645d0b1cb29be0cfec5555fa48badc2451aeb
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-03-04 21:37:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
                 index=0,
             )                
         with tg:
-            st.session_state.key_tab=pd.read_csv('./api_key_table.csv')  
+            st.session_state.key_tab=pd.read_csv('./data/api_key_table.csv')  
             select_key = st.selectbox("Key Index",st.session_state.key_tab.key_index.to_list())
             pw = st.text_input("password",'',type='password')
             # togg=st.toggle("***Record Chat***", help="⚠️ 회사 네트워크에서 보안 제한 - 외부 네트워크에서 사용해주세요")
